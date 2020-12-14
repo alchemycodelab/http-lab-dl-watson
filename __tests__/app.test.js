630ceee8c335ab3fb2e2700f25fce6c987f09c52
dl-watson <dlarkinwatson@gmail.com>
2020-12-14 15:14:36 -0800

    setting up tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,4 +2,10 @@
 const request = require("supertest");
 const app = require("../lib/app");
 
-describe("app routes", () => {});
+describe("app routes", () => {
+  it("returns hi", async () => {});
+  it("returns status code 200 and plain text with the request body", async () => {});
+  it("returns HTML with an h1 and the word red", async () => {});
+  it("returns HTML with an h1 and the word blue", async () => {});
+  it("returns HTML with an h1 and the word green", async () => {});
+});
